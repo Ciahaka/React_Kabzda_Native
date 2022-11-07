1965c20d3389c6e7eb0d63aa2052e53c5514a672
Ciahaka <wilddiver.by@gmail.com>
2022-11-07 13:30:31 +0300

    change app and index

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {sum} from "./01_testHello/01_ts";
+
+let  a =sum
+
 
 //      !данные перехали в тесты
 // export const render = 'Привет мой юный друх!'

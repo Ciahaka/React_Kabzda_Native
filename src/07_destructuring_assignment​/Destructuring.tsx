36d9ee6708d4c012f9537ff1f9358accb1439287
Ciahaka <wilddiver.by@gmail.com>
2022-11-15 14:45:29 +0300

    change Destructuring.tsx

--- src/07_destructuring_assignment​/Destructuring.tsx
+++ src/07_destructuring_assignment​/Destructuring.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
-export const Destructuring = () => {
+export type ManType = {
+  name: string
+  age: number
+  classes: Array<{ title: string }>
+  address: {
+    street: string
+  }
+}
+type PropsType = {
+  title: string
+  man: ManType
+}
+
+export const MansComponent: React.FC<PropsType> = ({title, man}) => {
+  // const {title, man} = boom
+
   return (
     <div>
-
+      <h1>{title}</h1>
+      <hr/>
+      <div>{man.name}</div>
     </div>
   );
 };

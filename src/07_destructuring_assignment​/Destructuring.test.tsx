fb3635394d9bb6f0b4c3217633d632a542d77e6d
Ciahaka <wilddiver.by@gmail.com>
2022-11-15 13:43:01 +0300

    add Destructuring.test.tsx

---
+++ src/07_destructuring_assignment​/Destructuring.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+type ManType={
+  name: string
+  age: number
+  classes: Array<{title:string}>
+  address:{
+    street:string
+  }
+}
+
+let man:ManType
+
+beforeEach(() =>{
+  let man = {
+    name: 'Alex',
+    age: 41,
+    classes: [{title_1: '1'}, {title_2: '2'}],
+    address:{
+      street:'Uskova'
+    }
+}})
+
+test('', () => {
+
+  const {name,age, classes, address} = man
+  const{street}=man.address
+  // const[title_1, title_2]=man.classes
+
+
+  expect(name).toBe('Alex')
+  expect(age).toBe(41)
+  // expect(classes.title_1).toBe('1')
+  expect(street).toBe('Uskova')
+  // expect(title_2).toBe('2')
+
+})

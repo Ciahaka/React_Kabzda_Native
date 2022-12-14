4ca6b01481f75acb7447f2616c35938fb19141f1
Ciahaka <wilddiver.by@gmail.com>
2022-12-15 00:08:06 +0300

    finish day

--- src/10_immutability/Immutability.test.tsx
+++ src/10_immutability/Immutability.test.tsx
@@ -1,8 +1,36 @@
 import {UserType} from './Immutability';
 
-export const hairDresser = (us: UserType, power: number) => {
-  us.hair = us.hair / power
-}
-test('there is less hair', ()=>{
+export const hairDresser = (us: UserType, power: number) => ({...us, hair: us.hair / power})
+
+test('there is less hair', () => {
+
+  let shaggyUser: UserType = {
+    name: 'Alex',
+    hair: 32,
+    address: {
+      title: 'Minsk'
+    }
+  }
+  const trimmedUser = hairDresser(shaggyUser, 2)
+  expect(trimmedUser.hair).toBe(16)
+  expect(shaggyUser.hair).toBe(32)
+  expect(trimmedUser.address).toBe(shaggyUser.address) //свойства вложенных объектов равны.
+
+
+})
+export const relocateUser = (us: UserType,newTitle:string)=>({...us, address: {...us.address,title:newTitle}})
+
+test('change address', () => {
+
+  let shaggyUser: UserType = {
+    name: 'Alex',
+    hair: 32,
+    address: {
+      title: 'Minsk'
+    }
+  }
+  const newRelocate = relocateUser(shaggyUser, 'Polotck')
+  expect(newRelocate.address.title).toBe('Polotck')
+  expect(shaggyUser.address.title).toBe('Minsk')
 
 })
--- src/10_immutability/Immutability.tsx
+++ src/10_immutability/Immutability.tsx
@@ -1,5 +1,13 @@
+import React from 'react';
+
 export type UserType={
   name:string
   hair:number
   address:{title:string}
 }
+/**
+ Ниже приведён пример чистой функции.Не мутирует приходящие данные, создаёт копию и работает с ней. Ретурнит результат обработкию Не создаёт внешних сайд эффектов
+ */
+// export const hairDresser = (us: UserType, power: number) => ({...us, hair: us.hair / power})
+
+// export const relocateUser = (us: UserType,newTitle:string)=>({...us, address: {...us.address,title:newTitle}})
